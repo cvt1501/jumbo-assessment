5ba94e58e5b382679e9df82c5abd2fba0189ef0b
Kevin Cavenatti <51295661+cvt1501@users.noreply.github.com>
2025-05-25 20:47:50 +0200

    Update StoreLoadException.java

--- src/main/java/com/jumbo/assessment/infrastructure/exception/StoreLoadException.java
+++ src/main/java/com/jumbo/assessment/infrastructure/exception/StoreLoadException.java
@@ -1,9 +1,7 @@
 package com.jumbo.assessment.infrastructure.exception;
 
 public class StoreLoadException extends RuntimeException {
-
     public StoreLoadException(String message) {
         super(message);
     }
-
 }
